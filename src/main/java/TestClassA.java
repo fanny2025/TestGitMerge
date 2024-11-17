d3c789298cc8e26c96712398ac23eac54fd3d951
Fanny <>
2024-11-17 10:46:22 -0800

    init commit

---
+++ src/main/java/TestClassA.java
@@ -0,0 +1,9 @@
+public class TestClassA {
+    public void method1() {
+        System.out.println("test class A: method 1");
+    }
+
+    public void method2() {
+        System.out.println("test class A: method 2");
+    }
+}
