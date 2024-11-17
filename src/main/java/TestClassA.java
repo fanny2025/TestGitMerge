eb8e0987ead49216c74728515b7d3cafe135589d
Fanny <>
2024-11-17 11:11:18 -0800

    main update

--- src/main/java/TestClassA.java
+++ src/main/java/TestClassA.java
@@ -1,9 +1,9 @@
 public class TestClassA {
     public void method1() {
-        System.out.println("test class A: method 1");
+        System.out.println("test class A: method 1, test - modify");
+        System.out.println("test - add");
     }
 
     public void method2() {
-        System.out.println("test class A: method 2");
     }
 }
